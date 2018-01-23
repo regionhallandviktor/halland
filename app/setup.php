6060da2026217c6302a4224d2ebea4671f4a4653
David Öhlin <hello@davidohl.in>
2018-01-23 11:25:02 +0100

    add component library and register sidebars

--- app/setup.php
+++ app/setup.php
@@ -11,86 +11,85 @@ use Roots\Sage\Template\BladeProvider;
  * Theme assets
  */
 add_action('wp_enqueue_scripts', function () {
-    wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
-    wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], null, true);
+	wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
+	wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], null, true);
 
-    // Enqueue Region Halland Component Library
-    wp_enqueue_style('rh-components', '//regionhalland.github.io/styleguide/dist/css/main.min.css', false, null);
+	// Enqueue Region Halland Component Library
+	wp_enqueue_style('rh-components', '//regionhalland.github.io/styleguide/dist/css/main.min.css', false, null);
 }, 100);
-
 
 
 /**
  * Theme setup
  */
 add_action('after_setup_theme', function () {
-    /**
-     * Enable features from Soil when plugin is activated
-     * @link https://roots.io/plugins/soil/
-     */
-    add_theme_support('soil-clean-up');
-    add_theme_support('soil-jquery-cdn');
-    add_theme_support('soil-nav-walker');
-    add_theme_support('soil-nice-search');
-    add_theme_support('soil-relative-urls');
+	/**
+	 * Enable features from Soil when plugin is activated
+	 * @link https://roots.io/plugins/soil/
+	 */
+	add_theme_support('soil-clean-up');
+	add_theme_support('soil-jquery-cdn');
+	add_theme_support('soil-nav-walker');
+	add_theme_support('soil-nice-search');
+	add_theme_support('soil-relative-urls');
 
-    /**
-     * Enable plugins to manage the document title
-     * @link https://developer.wordpress.org/reference/functions/add_theme_support/#title-tag
-     */
-    add_theme_support('title-tag');
+	/**
+	 * Enable plugins to manage the document title
+	 * @link https://developer.wordpress.org/reference/functions/add_theme_support/#title-tag
+	 */
+	add_theme_support('title-tag');
 
-    /**
-     * Register navigation menus
-     * @link https://developer.wordpress.org/reference/functions/register_nav_menus/
-     */
-    register_nav_menus([
-        'primary_navigation' => __('Primary Navigation', 'sage')
-    ]);
+	/**
+	 * Register navigation menus
+	 * @link https://developer.wordpress.org/reference/functions/register_nav_menus/
+	 */
+	register_nav_menus([
+		'primary_navigation' => __('Primary Navigation', 'sage')
+	]);
 
-    /**
-     * Enable post thumbnails
-     * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-     */
-    add_theme_support('post-thumbnails');
+	/**
+	 * Enable post thumbnails
+	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+	 */
+	add_theme_support('post-thumbnails');
 
-    /**
-     * Enable HTML5 markup support
-     * @link https://developer.wordpress.org/reference/functions/add_theme_support/#html5
-     */
-    add_theme_support('html5', ['caption', 'comment-form', 'comment-list', 'gallery', 'search-form']);
+	/**
+	 * Enable HTML5 markup support
+	 * @link https://developer.wordpress.org/reference/functions/add_theme_support/#html5
+	 */
+	add_theme_support('html5', ['caption', 'comment-form', 'comment-list', 'gallery', 'search-form']);
 
-    /**
-     * Enable selective refresh for widgets in customizer
-     * @link https://developer.wordpress.org/themes/advanced-topics/customizer-api/#theme-support-in-sidebars
-     */
-    add_theme_support('customize-selective-refresh-widgets');
+	/**
+	 * Enable selective refresh for widgets in customizer
+	 * @link https://developer.wordpress.org/themes/advanced-topics/customizer-api/#theme-support-in-sidebars
+	 */
+	add_theme_support('customize-selective-refresh-widgets');
 
-    /**
-     * Use main stylesheet for visual editor
-     * @see resources/assets/styles/layouts/_tinymce.scss
-     */
-    add_editor_style(asset_path('styles/main.css'));
+	/**
+	 * Use main stylesheet for visual editor
+	 * @see resources/assets/styles/layouts/_tinymce.scss
+	 */
+	add_editor_style(asset_path('styles/main.css'));
 }, 20);
 
 /**
  * Register sidebars
  */
 add_action('widgets_init', function () {
-    $config = [
-        'before_widget' => '<section class="widget %1$s %2$s">',
-        'after_widget'  => '</section>',
-        'before_title'  => '<h3>',
-        'after_title'   => '</h3>'
-    ];
-    register_sidebar([
-        'name'          => __('Primary', 'sage'),
-        'id'            => 'sidebar-primary'
-    ] + $config);
-    register_sidebar([
-        'name'          => __('Footer', 'sage'),
-        'id'            => 'sidebar-footer'
-    ] + $config);
+	$config = [
+		'before_widget' => '<section class="widget %1$s %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h3>',
+		'after_title'   => '</h3>'
+	];
+	register_sidebar([
+		'name'          => __('Left', 'sage'),
+		'id'            => 'sidebar-left'
+	] + $config);
+	register_sidebar([
+		'name'          => __('Right', 'sage'),
+		'id'            => 'sidebar-right'
+	] + $config);
 });
 
 /**
@@ -98,36 +97,36 @@ add_action('widgets_init', function () {
  * Note: updated value is only available for subsequently loaded views, such as partials
  */
 add_action('the_post', function ($post) {
-    sage('blade')->share('post', $post);
+	sage('blade')->share('post', $post);
 });
 
 /**
  * Setup Sage options
  */
 add_action('after_setup_theme', function () {
-    /**
-     * Add JsonManifest to Sage container
-     */
-    sage()->singleton('sage.assets', function () {
-        return new JsonManifest(config('assets.manifest'), config('assets.uri'));
-    });
+	/**
+	 * Add JsonManifest to Sage container
+	 */
+	sage()->singleton('sage.assets', function () {
+		return new JsonManifest(config('assets.manifest'), config('assets.uri'));
+	});
 
-    /**
-     * Add Blade to Sage container
-     */
-    sage()->singleton('sage.blade', function (Container $app) {
-        $cachePath = config('view.compiled');
-        if (!file_exists($cachePath)) {
-            wp_mkdir_p($cachePath);
-        }
-        (new BladeProvider($app))->register();
-        return new Blade($app['view']);
-    });
+	/**
+	 * Add Blade to Sage container
+	 */
+	sage()->singleton('sage.blade', function (Container $app) {
+		$cachePath = config('view.compiled');
+		if (!file_exists($cachePath)) {
+			wp_mkdir_p($cachePath);
+		}
+		(new BladeProvider($app))->register();
+		return new Blade($app['view']);
+	});
 
-    /**
-     * Create @asset() Blade directive
-     */
-    sage('blade')->compiler()->directive('asset', function ($asset) {
-        return "<?= " . __NAMESPACE__ . "\\asset_path({$asset}); ?>";
-    });
+	/**
+	 * Create @asset() Blade directive
+	 */
+	sage('blade')->compiler()->directive('asset', function ($asset) {
+		return "<?= " . __NAMESPACE__ . "\\asset_path({$asset}); ?>";
+	});
 });
--- resources/views/template-custom.blade.php
+++ resources/views/template-custom.blade.php
@@ -5,8 +5,16 @@
 @extends('layouts.app')
 
 @section('content')
+	@if (is_active_sidebar('sidebar-right'))
+		@include('partials.sidebar-right')
+	@endif
+
 	@while(have_posts()) @php(the_post())
 		@include('partials.page-header')
 		@include('partials.content-page')
 	@endwhile
+
+	@if (is_active_sidebar('sidebar-left'))
+		@include('partials.sidebar-left')
+	@endif
 @endsection
