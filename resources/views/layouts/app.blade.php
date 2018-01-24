b67ceb6424dad5458a8363c7e22600fb464ba85f
David Öhlin <hello@davidohl.in>
2018-01-24 17:00:35 +0100

    center container and add grid to custom template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,16 +4,11 @@
 	<body @php(body_class())>
 		@php(do_action('get_header'))
 		@include('partials.header')
-		<div class="wrap container" role="document">
+		<div class="container mx-auto" role="document">
 			<div class="content">
 				<main class="main">
 					@yield('content')
 				</main>
-				@if (App\display_sidebar())
-					<aside class="sidebar">
-						@include('partials.sidebar-left')
-					</aside>
-				@endif
 			</div>
 		</div>
 		@php(do_action('get_footer'))
--- resources/views/template-custom.blade.php
+++ resources/views/template-custom.blade.php
@@ -5,16 +5,26 @@
 @extends('layouts.app')
 
 @section('content')
-	@if (is_active_sidebar('sidebar-right'))
-		@include('partials.sidebar-right')
-	@endif
-
-	@while(have_posts()) @php(the_post())
-		@include('partials.page-header')
-		@include('partials.content-page')
-	@endwhile
-
+	
+	
 	@if (is_active_sidebar('sidebar-left'))
-		@include('partials.sidebar-left')
+		<div class="col col-12 md-col-3 px2">
+			@include('partials.sidebar-left')
+		</div>
 	@endif
+	
+
+	<div class="col col-12 md-col-6 px2">
+		@while(have_posts()) @php(the_post())
+			@include('partials.page-header')
+			@include('partials.content-page')
+		@endwhile
+	</div>
+
+	@if (is_active_sidebar('sidebar-right'))
+		<div class="col col-12 md-col-3 px2">
+			@include('partials.sidebar-right')
+		</div>
+	@endif
+
 @endsection
