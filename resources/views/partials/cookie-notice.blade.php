9333d1091303c9b2325d0a6088788b6e6ea3d7b0
David Öhlin <hello@davidohl.in>
2018-01-31 08:50:02 +0100

    add demo styles

--- resources/views/partials/cookie-notice.blade.php
+++ resources/views/partials/cookie-notice.blade.php
@@ -1,6 +1,14 @@
 @if (isset($cookie_notice))
-	<div id="cookie-notice" class="notice">
-		<p class="notice__text">{!! $cookie_notice['message'] !!}</p>
-		<button class="notice__button" id="cookie-consent">{!! $cookie_notice['button'] !!}</button>
+	<div id="cookie-notice" class="notice background-dark-blue p2">
+		<div class="container mx-auto">
+			<div class="col-12 px2">
+				<div class="notice__text text-white">
+					{!! $cookie_notice['message'] !!}
+				</div>
+				<button id="cookie-consent" class="strong text-white notice__button">
+					{!! $cookie_notice['button'] !!}
+				</button>
+			</div>
+		</div>
 	</div>
 @endif
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,12 @@
 <header class="banner">
-	<div class="container">
-		<a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-		<nav class="nav-primary">
-			@if (has_nav_menu('main-menu'))
-				{!! wp_nav_menu(['theme_location' => 'main-menu', 'menu_class' => 'nav']) !!}
-			@endif
-		</nav>
+	<div class="container mx-auto">
+		<div class="col col-12 px2 mb4">
+			<a class="brand strong" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+			<nav class="nav-primary">
+				@if (has_nav_menu('main-menu'))
+					{!! wp_nav_menu(['theme_location' => 'main-menu', 'menu_class' => 'nav']) !!}
+				@endif
+			</nav>
+		</div>
 	</div>
 </header>
