d99f7e0f360ffbf09a63976970128e2a4b05313b
David Öhlin <hello@davidohl.in>
2018-02-05 15:53:44 +0100

    wrap content in article class

--- resources/views/template-custom.blade.php
+++ resources/views/template-custom.blade.php
@@ -16,7 +16,7 @@
 
 	<div class="col col-12 md-col-6 px2">
 		@while(have_posts()) @php(the_post())
-			@include('partials.page-header')
+			{{-- @include('partials.page-header') --}}
 			@include('partials.content-page')
 		@endwhile
 	</div>
