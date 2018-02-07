7cf57db94ee2c60f1179c133610aa131440bb2d3
David Öhlin <hello@davidohl.in>
2018-02-07 11:28:53 +0100

    add 'addItem' method

--- app/theme/Breadcrumbs.php
+++ app/theme/Breadcrumbs.php
@@ -26,19 +26,15 @@ class Breadcrumbs
 		$post_type = get_post_type_object($post->post_type);
 		$breadcrumbs = array();
 		
-		$breadcrumbs[] = array(
-			'name' => __('Home'),
-			'url' => get_home_url()
-		);
+		$breadcrumbs = $this->addItem($breadcrumbs, __('Home'), get_home_url());
 		
 
 		if (is_single() && $post_type->has_archive) {
-			$cpt_archive_link = (is_string($post_type->has_archive)) ? get_permalink(get_page_by_path($post_type->has_archive)) : get_post_type_archive_link($post_type->name);
+			$cpt_archive_link = (is_string($post_type->has_archive)) ? 
+				get_permalink(get_page_by_path($post_type->has_archive)) : 
+				get_post_type_archive_link($post_type->name);
 
-			$breadcrumbs[] = array(
-				'name' => $post_type->label,
-				'url' => $cpt_archive_link
-			);
+			$breadcrumbs = $this->addItem($breadcrumbs, $post_type->label, $cpt_archive_link);
 		}
 
 		if (is_page() || (is_single() && $post_type->hierarchical == true)) {
@@ -49,22 +45,14 @@ class Breadcrumbs
 					foreach ($anc as $ancestor) {
 						if (get_post_status($ancestor) != 'private') {
 
-							$breadcrumbs[] = array(
-								'name' => get_the_title($ancestor),
-								'url' => get_permalink($ancestor)
-							);
+							$breadcrumbs = $this->addItem($breadcrumbs, get_the_title($ancestor), get_permalink($ancestor));
 						}
 					}
 					
-					$breadcrumbs[] = array(
-						'name' => $title,
-						'url' => false
-					);
+					$breadcrumbs = $this->addItem($breadcrumbs, $title, false);
+
 				} else {
-					$breadcrumbs[] = array(
-						'name' => get_the_title(),
-						'url' => false
-					);
+					$breadcrumbs = $this->addItem($breadcrumbs, get_the_title(), false);
 				}
 			} else {
 				if (is_home()) {
@@ -78,12 +66,25 @@ class Breadcrumbs
 				} else {
 					$title = get_the_title();
 				}
-				$breadcrumbs[] = array(
-					'name' => get_the_title(),
-					'url' => false
-			);
+				
+				$breadcrumbs = $this->addItem($breadcrumbs, $title, false);
 		}
 
 		return $breadcrumbs;
-    }
+	}
+
+	/**
+	 * Returns the tree for breadcrumbs.
+	 * @param array $list 
+	 * @param string $name
+	 * @param string|boolean $url
+	 * @return array
+	 */
+	public function addItem($list, $name, $url) {
+		$list[] = array(
+			'name' => $name,
+			'url' => $url
+		);
+		return $list;
+	}
 }
