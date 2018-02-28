f330fb158eb6eca67118af9e0613f575a88478f4
Viktor Sarge <viktor.sarge@regionhalland.se>
2018-02-28 15:57:30 +0100

    Nav reimplementation as mobile first WIP

--- resources/views/partials/main-nav.blade.php
+++ resources/views/partials/main-nav.blade.php
@@ -1,29 +1,43 @@
 
 	<header class="container mx-auto">
-		<div class="col col-12 px2 mb4" id="main-nav-container">
-			<div class="burgermenu" id="burgermenu">
-				<a class="brand strong nav-logo" href="#">
+		<div class="col col-12 px2 mb4 main-nav-container" id="main-nav-container">
+			<div class="burgermenu">
+				<a class="nav-logo" href="#">
 					<img class="burgermenu-icon" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/black/menu.png" alt="Meny">
 					Meny
 				</a>
 			</div>
-			<a class="brand strong nav-logo" id="nav-logo" href="{{ home_url('/') }}">
-				<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/RH-logo.jpg" alt="Logotyp"></a>
-			<span class="brand" id="brand-name">{{ get_bloginfo('name', 'display') }}</span>
-			<span class="brand nav-separator">|</span>
-			<nav class="nav-primary" id="nav-primary">
+			<a class="brand strong nav-logo" href="{{ home_url('/') }}">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/RH-logo.jpg" alt="Logotyp">
+			</a>
+			<div class="nav-tool nav-tool-search">
+				<img class="nav-tool-icon" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/black/search.png" alt="Sök">
+				<span class="nav-icons-label">
+					Sök
+				</span>
+			</div>
+			<span class="brand brand-name">
+				{{ get_bloginfo('name', 'display') }}
+			</span>
+			<span class="brand nav-separator">
+				|
+			</span>
+			<nav class="nav-primary">
 				@if (has_nav_menu('main-menu'))
 					{!! wp_nav_menu(['theme_location' => 'main-menu', 'menu_class' => 'nav']) !!}
 				@endif
 			</nav>
-			<div class="nav-tool" id="nav-search-tool">
-				<img class="nav-tool-icon" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/black/search.png" alt="Sök"><span class="nav-icons-label">Sök</span>
+			<div class="nav-tool nav-language-tool">
+				<img class="nav-tool-icon" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/black/language.png" alt="Språk">
+				<span class="nav-icons-label">
+					Språk
+				</span>
 			</div>
-			<div class="nav-tool" id="nav-language-tool">
-				<img class="nav-tool-icon" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/black/language.png" alt="Språk"><span class="nav-icons-label">Språk</span>
-			</div>
-			<div class="nav-tool" id="nav-login-tool">
-				<img class="nav-tool-icon" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/black/profile.png" alt="Profil"><span class="nav-icons-label">Logga in</span>
+			<div class="nav-tool nav-login-tool">
+				<img class="nav-tool-icon" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/black/profile.png" alt="Profil">
+				<span class="nav-icons-label">
+					Logga in
+				</span>
 			</div>
 			</div>
 		</div>
