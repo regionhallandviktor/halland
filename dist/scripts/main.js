c346c535d53005f005190711f1e284fd89645cc7
Viktor Sarge <viktor.sarge@regionhalland.se>
2018-03-01 12:56:13 +0100

    Nav: fast toggle restored in burger menu

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -462,7 +462,7 @@ Router.prototype.loadEvents = function loadEvents () {
   finalize: function finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
     $( ".burgermenu" ).click(function() {
-        $(".nav-primary, .nav-tool-language, .nav-tool-login").toggle(5000);
+        $(".nav-primary, .nav-tool-language, .nav-tool-login").toggle("fast");
 	});
   },
 });
--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -5,7 +5,7 @@ export default {
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
     $( ".burgermenu" ).click(function() {
-        $(".nav-primary, .nav-tool-language, .nav-tool-login").toggle(5000);
+        $(".nav-primary, .nav-tool-language, .nav-tool-login").toggle("fast");
 	});
   },
 };
