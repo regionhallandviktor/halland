e04dd811d3f6e7789b20e327c76b08979472f835
David Öhlin <hello@davidohl.in>
2018-03-06 08:05:56 +0100

    refactoring

--- app/controllers/app.php
+++ app/controllers/app.php
@@ -6,9 +6,38 @@ use Sober\Controller\Controller;
 
 class App extends Controller
 {
+
+	/**
+	 * Returns the name of the site
+	 * @return string
+	 */
 	public function siteName()
 	{
 		return get_bloginfo('name');
+	}
+
+	/**
+	 * Returns the title of the current page
+	 * @return string
+	 */
+	public function title()
+	{
+		if (is_home()) {
+			if ($home = get_option('page_for_posts', true)) {
+				return get_the_title($home);
+			}
+			return __('Latest Posts', 'sage');
+		}
+		if (is_archive()) {
+			return get_the_archive_title();
+		}
+		if (is_search()) {
+			return sprintf(__('Search Results for %s', 'sage'), get_search_query());
+		}
+		if (is_404()) {
+			return __('Not Found', 'sage');
+		}
+		return get_the_title();
 	}
 
 	/**
@@ -31,26 +60,6 @@ class App extends Controller
 		$menu = new \App\Theme\Navigation();
 		$menu = $menu->getSidebarMenu();
 		return $menu;
-	}
-
-	public static function title()
-	{
-		if (is_home()) {
-			if ($home = get_option('page_for_posts', true)) {
-				return get_the_title($home);
-			}
-			return __('Latest Posts', 'sage');
-		}
-		if (is_archive()) {
-			return get_the_archive_title();
-		}
-		if (is_search()) {
-			return sprintf(__('Search Results for %s', 'sage'), get_search_query());
-		}
-		if (is_404()) {
-			return __('Not Found', 'sage');
-		}
-		return get_the_title();
 	}
 	
 	/**
