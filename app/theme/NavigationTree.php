40a2104a1f0feb1b408bed014c9856038d073697
David Öhlin <hello@davidohl.in>
2018-03-22 08:23:37 +0100

    wip: navtree

--- app/theme/NavigationTree.php
+++ app/theme/NavigationTree.php
@@ -4,156 +4,74 @@ namespace App\Theme;
 
 class NavigationTree
 {
-    // public $args = array();
-
-    // protected $postStatuses = array('publish');
-    // protected $ancestors = null;
-
-    // public $topLevelPages = null;
-    // public $secondLevelPages = null;
-    public $sortedPages = null;
-
     public function __construct($args = array(), $parent = false)
     { 
-       //if ($parent) {
-       //    $parent = get_post($parent);
-       //    $this->isAjaxParent = true;
-       //}
-
-       //// Get valuable page information
-       //if ($parent) {
-       //    $parent->post_parent = 0;
-       //    $this->currentPage = $parent;
-       //} else {
-       //    $this->currentPage = $this->getCurrentPage();
-       //}
-
-       //// Merge args
-       //$this->args = array_merge(array(
-       //    'theme_location' => '',
-       //    'include_top_level' => false,
-       //    'sublevel' => false,
-       //    'top_level_type' => 'tree',
-       //    'render' => 'active',
-       //    'depth' => -1,
-       //    'start_depth' => 1,
-       //    'classes' => 'nav',
-       //    'id' => '',
-       //    'sidebar' => false
-       //), $args);
-
-       //
-       //if ($this->args['depth'] > -1 && $this->args['start_depth'] > 1) {
-       //    $this->args['depth'] += $this->args['start_depth'];
-       //}
-
-       //if (is_user_logged_in()) {
-       //    $this->postStatuses[] = 'private';
-       //}
-
-        // $this->getTopLevelPages();
-        // $this->getSecondLevelPages();
-        $this->sortPages();
-        
-    }
-
-    /**
-     * Gets top level pages
-     * @return void
-     */
-    public function sortPages()
-    {   
         $pages = get_pages();
-        $sorted = array();
-
-        // Push parents to $sorted
-        foreach($pages as $page) {
-            if ($page->post_parent === 0) {
-                $sorted[$page->ID] = $page;
-            }
-        }
-
-
-
-        $this->sortedPages = $sorted;
-        return $this->sortedPages;
+        return $this->sortPages($pages, 0);
     }
 
+    public function sortPages($pages)
+    {   
+        $new = array();
+        
+        foreach ($pages as $page){
+            $new[$page->post_parent][] = $page;
+        }
+        
+        $tree = $this->createTree($new, $new[0]); // changed
+
+        var_dump($tree);
+    }
+
+    public function createTree(&$pages, $parents) {
+        $tree = array();
+
+        foreach ($parents as $key => $parent) {
+            
+            if (isset($pages[$parent->ID])) {
+                $parent->children = $pages[$parent->ID];
+            }
+            $tree[] = $parent;
+        } 
+
+        return $tree;
+    }
+
+    // public function createTree(&$list, $pages) {
+    //     $tree = array();
+    //     var_dump($pages);
+    //     foreach ($pages as $key => $page) {
+    //         if (!isset($list[$page->ID]) || gettype($list[$page->ID]) !== 'array') {
+    //             return false;
+    //         }
+
+    //         if (isset($pages[$page->post_parent])) {
+    //             $page['children'] = $this->createTree($list, $list[$page->ID]);
+    //         }
+
+    //         $tree[] = $page;
+    //     } 
+    //     return $tree;
+    // }
     /**
      * Gets top level pages
      * @return void
      */
-    //protected function getTopLevelPages()
-    //{   
-    //    $topLevelQuery = new \WP_Query(array(
-    //        'post_parent' => 0,
-    //        'post_type' => 'page',
-    //        'post_status' => $this->postStatuses,
-    //        'orderby' => 'menu_order post_title',
-    //        'order' => 'asc',
-    //        'posts_per_page' => -1,
-    //        'meta_query'    => array(
-    //            'relation' => 'AND',
-    //            array(
-    //                'relation' => 'OR',
-    //                array(
-    //                    'key' => 'hide_in_menu',
-    //                    'compare' => 'NOT EXISTS'
-    //                ),
-    //                array(
-    //                    'key'   => 'hide_in_menu',
-    //                    'value' => '0',
-    //                    'compare' => '='
-    //                )
-    //            )
-    //        )
-    //    ));
+//     public function sortPages($pages, $parentId)
+//     {   
+//         $branch = array();
 
-    //    $this->topLevelPages = $topLevelQuery->posts;
-    //    return $this->topLevelPages;
-    //}
+//         foreach ($pages as $page) {
+//             if ($page->post_parent === $parentId) {
+//                 $children = $this->sortPages( $pages, $page->ID );
+//                 if ($pages) {
+//                     $pages['wpse_children'] = $children;
+//                 }
+//                 $branch[$page->ID] = $page;
+//                 unset( $page );
+//             }
+//         }   
 
-    /**
-     * Gets second level pages
-     * @return array
-     */
-    // protected function getSecondLevelPages()
-    // {
-    //     $secondLevel = array();
-
-    //     foreach ($this->topLevelPages as $topLevelPage) {
-    //         $pages = get_posts(array(
-    //             'post_parent' => $topLevelPage->ID,
-    //             'post_type' => 'page',
-    //             'orderby' => 'menu_order post_title',
-    //             'order' => 'asc',
-    //             'posts_per_page' => -1
-    //         ));
-
-    //         $secondLevel[$topLevelPage->ID] = $pages;
-    //     }
-
-    //     $this->secondLevelPages = $secondLevel;
-    //     return $secondLevel;
-    // }
-
-    // /**
-    //  * Gets the current page object
-    //  * @return object
-    //  */
-    // protected function getCurrentPage()
-    // {
-    //     if (is_post_type_archive()) {
-    //         $pageForPostType = get_option('page_for_' . get_post_type());
-    //         return get_post($pageForPostType);
-    //     }
-
-    //     global $post;
-
-    //     if (!is_object($post)) {
-    //         return get_queried_object();
-    //     }
-
-    //     return $post;
-    // }
+//         return $branch;
+//     }
 }
