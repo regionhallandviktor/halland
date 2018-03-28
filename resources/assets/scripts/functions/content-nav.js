023bf20ecdc6d22168921d92dd9987bdb99bd8ee
Joel Sandén <me@joelsanden.se>
2018-03-28 15:07:10 +0200

    content nav, not done

--- resources/assets/scripts/functions/content-nav.js
+++ resources/assets/scripts/functions/content-nav.js
@@ -7,42 +7,61 @@ class ContentNav {
 		this.menu = $(menu);
 		this.container = $(container);
 		this.tags = $(tags, this.container);
-		this.furnishMenu();
-		this.furnishTags();
-		this.createMenu();
+
+		this.slider;
+
+		this.makeTags();
+		this.makeMenu();
+
+		this.initScroll();
+
 	}
 
-	furnishMenu() {
+	changeSliderHeight(int) {
+		$(this.slider).height(int);
+	}
+
+	makeMenu() {
+		var ul, all_li;
+
 		this.menu.append('<span class="content-nav__heading">På denna sidan:</span>');
-		this.menu.append('<ul></ul>');
+		this.menu.append('<div class="clearfix content-nav__container"><div class="content-nav__slider"></div><ul class="content-nav__menu"></ul></div>');
 		this.menu.addClass('content-nav');
 
-		$(document).on('scroll', (index) => {
-			console.log('scrolling', index);
-		})
+		ul = $('.content-nav__menu', this.menu);
+		$.each(this.tags, (index, e) => {
+			ul.append('<li class="content-nav__item"><a href="#'+ $(e).attr('id') +'">'+ $(e).text() +'</a></li>');
+		});	
+
+		all_li = $('li', ul);
+		console.log(all_li[0]);
+
+		this.slider = $('.content-nav__slider', this.menu);
+		this.changeSliderHeight($(all_li[0]).height());
 	}
-	
-	furnishTags() {
-		this.tags.each( (index) => {
-			$( this ).attr('id', 'contentNav_'+index);
+
+	makeTags() {
+		this.tags.each( (index, e) => {
+			$(e).attr('id', 'contentNav_'+index);
 		});
 	}
 
-	createMenu() {
-		this.ul = $('ul', this.menu);
-		$.each(this.tags, (index, e) => {
-			this.ul.append('<li class="content-nav__item"><a href="#'+ $(e).attr('id') +'">'+ $(e).text() +'</a></li>');
-		});	
+	initScroll() {
+		var containerHeight = this.container.height();
+		var containerOffset = this.container.position().top;
+		var menuHeight = this.menu.height();
+		var scrollPos = $(document).scrollTop();
+
+
+
+		$(document).on('scroll', () => {
+			scrollPos = $(document).scrollTop();
+
+			var percentage = ((scrollPos - containerOffset) / containerHeight);
+			this.slider.css('margin-top', menuHeight * percentage);
+		})
 	}
 
-	bind() {
-		/*
-		this.consentBtn.on('click', () => {
-			this.removeNotice();
-			this.setCookie(this.name);
-		})
-		*/
-	}
 }
 
 export default ContentNav;
