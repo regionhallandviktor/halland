08a4a94377cfa3df0311c47dedc6c757f54bca98
David Öhlin <hello@davidohl.in>
2018-03-29 14:18:07 +0200

    disable entry meta, remove heading

--- resources/views/partials/article.blade.php
+++ resources/views/partials/article.blade.php
@@ -1,9 +1,6 @@
 <article @php(post_class())>
-	<header>
-		<h2 class="entry-title"><a href="{{ get_permalink() }}">{!! get_the_title() !!}</a></h2>
-	</header>
 	@php(the_content())
-	<hr>
+{{-- 	<hr>
 	@include('partials/entry-meta')
-	<hr>
+	<hr> --}}
 </article>
