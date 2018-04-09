f1c252fbc0e633040fcf5ef35ff34ba512a18aed
David Öhlin <hello@davidohl.in>
2018-04-09 13:46:36 +0200

    import content fields

--- app/Acf/Import.php
+++ app/Acf/Import.php
@@ -15,7 +15,8 @@ class Import
 				$acfExportManager->setTextdomain('halland');
 				$acfExportManager->setExportFolder(__DIR__);
 				$acfExportManager->autoExport(array(
-				    'options-theme-cookie-notice' => 'group_5aa63e3f4d0c8'
+				    'options-theme-cookie-notice' => 'group_5aa63e3f4d0c8',
+				    'options-theme-data-curator' => 'group_5ac48d9ea70de'
 				));
 				$acfExportManager->import();
 			}
